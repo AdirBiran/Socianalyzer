90e500ed19e5535f935e4a7a4a2c2270500d6107
liatico <liatp@post.bgu.ac.il>
2020-12-11 12:46:43 +0200

    added kmeans classifier

---
+++ Classifier3.py
@@ -0,0 +1,41 @@
+from Extractor import Extractor
+from Settings import *
+import numpy as np
+from shutil import copyfile
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import pandas as pd
+
+ext = Extractor()
+dirPath = FACES_PATH
+ext.extractFeaturesFromDirectory(dirPath)
+
+
+class Classifier:
+    def __init__(self, face_images_list):
+        self.face_images_list = face_images_list
+        self.face_images_encoded_list = []
+        for face_image in face_images_list:
+            self.face_images_encoded_list.append(face_image.encoding)
+
+    def calculate_all_similarities(self):
+        # Fitting K-Means to the dataset
+        dataset1 =pd.DataFrame()
+        dataset1['Image'] = self.face_images_list
+        # find out how to estimate number of clusters
+        kmeans = KMeans(n_clusters=18, init='k-means++', random_state=42)
+        y_kmeans = kmeans.fit_predict(self.face_images_encoded_list)
+        # beginning of  the cluster numbering with 1 instead of 0
+        y_kmeans1 = y_kmeans + 1
+        # New Dataframe called cluster
+        cluster = pd.DataFrame(y_kmeans1)
+        # Adding cluster to the Dataset1
+        dataset1['cluster'] = cluster
+        for index, row in dataset1.iterrows():
+            # save image in directory of cluster's number
+            cluster_path = os.path.join(CLUSTERS_PATH, str(row['cluster']))
+            if not os.path.isdir(cluster_path):
+                os.mkdir(cluster_path)
+            copyfile(os.path.join(FACES_PATH, row['Image'].name),
+                     os.path.join(CLUSTERS_PATH, str(row['cluster']), row['Image'].name))
+
--- Main.py
+++ Main.py
@@ -1,9 +1,21 @@
 from Cropper import Cropper
 from Extractor import Extractor
-from Classifier2 import Classifier
+from Classifier3 import Classifier
 from Settings import *
+import os
 
 def start():
+    # create directories Data, Faces, Clusters:
+    try:
+        if not os.path.isdir('Data'):
+            os.mkdir('Data')
+        if not os.path.isdir('Faces'):
+            os.mkdir('Faces')
+        if not os.path.isdir('Clusters'):
+            os.mkdir('Clusters')
+    except OSError:
+        print("Creation of the directories failed")
+
     cropper = Cropper()
     cropper.cropImagesInDirectory(DATA_PATH)
 
